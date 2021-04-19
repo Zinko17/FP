bb07c6b183011f3a31f464786d6d624111a10b19
Zinko17 <vovazin2@gmail.com>
2021-04-19 18:27:40 +0600

    try and except

--- week3.py
+++ week3.py
@@ -5,5 +5,28 @@
 #     print('No')
 
 
+# numbers = [10,20,30,40,50,5,7,8,9]
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(number)
+
+# try:
+#     print('1' + 1)
+# except TypeError:
+#     print('Error')
+# finally:
+#     print('finally')
 
 
+# names = ['vladimir', 'aliya', 'begimai', 'erbol', 'cholpon', 'jamilya', 'bakyt']
+# salaries = [0, 10000, 20000, 30000]
+#
+# i = 0
+# while i < len(names):
+#     try:
+#         salaries[i] *= 2
+#     except IndexError:
+#         salaries.append(0)
+#     i += 1
+#
+# print(salaries)
