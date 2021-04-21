59a0755b156a0163d797b73e8829c1d221b19db7
Zinko17 <vovazin2@gmail.com>
2021-04-22 00:19:29 +0600

    added hw6

--- 21april.py
+++ 21april.py
@@ -12,5 +12,30 @@
 # print(iterat)
 
 
-file1 = open('text.txt', 'w')
-file1.write('hello')
+# file1 = open('text.txt', 'w')
+# file1.write('hello')
+
+
+# data = [
+#     {'text':'oh hi duuuude how r uy??check this 1xbet'},
+#     {'text':'Dear Harry Potter, i am Frodo Baggins 1xbet represent 1xbet company.Best bet service'},
+#     {'text':'wooooh yoow harry look at my jackpot 100000000$ at 1xbet service'},
+#     {'text':'Harry , today i saw the man who looks like Hawkeye from Avengers on 100% and he dont use 1xbet service'},
+# ]
+#
+# spam_word = None
+# qspam_word = 0
+# for i in data:
+#     data1 = i['text'].split()
+#     print(data1)
+#     for word in data1:
+#         quantity = data1.count(word)
+#         if quantity > qspam_word:
+#             qspam_word = quantity
+#             spam_word = word
+# print(spam_word)
+# final_mail = 'Hello Harry, my name is Maksim, Im  still waiting for the letter from Hogwarts'
+# if spam_word in final_mail:
+#     print('Final_mail is spam!!!')
+# else:
+#     print('Final_mail is not spam')
