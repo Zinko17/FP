ef1eaa2e3ba7968b0bb1ae7996f4ceda08be37c8
Zinko17 <vovazin2@gmail.com>
2021-04-23 18:32:10 +0600

    23 april

---
+++ 23april.py
@@ -0,0 +1,48 @@
+"""
+ТЗ:
+По данным введенным пользователем вычислить, сможет он купить выбранный им товар или нет.
+Если товар в списке отсутствует - NOT OK
+__________
+Входные данные: название товара,кол-во товара, наличные
+Реализовать 2+ функциями
+Выходные данные: словарь состящий из:
+{названия товара как ключ:кол-во, следующий элемент - потраченная сумма - ключ, значение сумма}
+"""
+
+
+# data = {
+#     'glock.20':2000,
+#     'usp':2500,
+#     'fs':3467,
+#     'deagle':5000,
+#     'p92':4000,
+#     'colt':90000,
+#     'magnum':6000,
+#     'p90':10000,
+#     'mp7':11000,
+#     'uzi':12000,
+#     'mp5':14000,
+#     'm16':20000,
+#     'ak-47':19000,
+#     'm416':24000,
+#     'famas':21000,
+#     'AWM':30000,
+#     'Dragunov':31000,
+#     'Barett':50000,
+#     'RPG':100000,
+#     'Topol-M':2000000
+# }
+# # payback = count_money(data[weapon_name],quntity,users_money)
+# # print(data[weapon])#цена
+# def count_money(price,weapon_q,users_money):
+#     return users_money - price * weapon_q
+#
+# weapon = 'AWM'
+# def buyer_dt(weapon_name,weapon_q,users_money):
+#     if weapon in data:
+#         if weapon_name == weapon:
+#             print(data[weapon])
+#         else:
+#             print('Такого оружия нет')
+# buyer_dt('AWM', 1, 35000)
+
