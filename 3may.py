1bcd67d3b88026563fde181b93f53851ba819af3
Zinko17 <vovazin2@gmail.com>
2021-05-03 18:32:51 +0600

    new thrms

---
+++ 3may.py
@@ -0,0 +1,16 @@
+# #Контекстный менеджер
+# with open('test.txt','r') as file2:
+#     print(file2.read())
+
+
+
+# import sys
+# countdown = int(sys.argv[1])
+# astronaut_name = sys.argv[2]
+# for i in range(1, countdown+1):
+#     print(i)
+#     if i == countdown:
+#         print('ПОЕХАЛИ! -',astronaut_name)
+
+
+
